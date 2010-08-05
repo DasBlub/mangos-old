0cfaef91c737266770f857a6b5056d8e0548a7f8
cipherCOM <github@ciphercom.de>
2010-08-05 16:48:33 +0200

    Added new UnorderedMap files

--- src/framework/Utilities/UnorderedSet.h
+++ src/framework/Utilities/UnorderedSet.h
@@ -16,39 +16,39 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef MANGOS_UNORDERED_MAP_H
-#define MANGOS_UNORDERED_MAP_H
+#ifndef MANGOS_UNORDERED_SET_H
+#define MANGOS_UNORDERED_SET_H
 
 #include "Platform/CompilerDefs.h"
 #include "Platform/Define.h"
 
 #if COMPILER == COMPILER_INTEL
-#include <ext/hash_map>
+#include <ext/hash_set>
 #elif COMPILER == COMPILER_GNU && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
-#include <tr1/unordered_map>
+#include <tr1/unordered_set>
 #elif COMPILER == COMPILER_GNU && __GNUC__ >= 3
-#include <ext/hash_map>
+#include <ext/hash_set>
 #elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1500 && _HAS_TR1   // VC9.0 SP1 and later
-#include <unordered_map>
+#include <unordered_set>
 #else
-#include <hash_map>
+#include <hash_set>
 #endif
 
 #ifdef _STLPORT_VERSION
-#define UNORDERED_MAP std::hash_map
-using std::hash_map;
+#define UNORDERED_SET std::hash_set
+using std::hash_set;
 #elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1500 && _HAS_TR1
-#define UNORDERED_MAP std::tr1::unordered_map
+#define UNORDERED_SET std::tr1::unordered_set
 #elif COMPILER == COMPILER_MICROSOFT && _MSC_VER >= 1300
-#define UNORDERED_MAP stdext::hash_map
-using stdext::hash_map;
+#define UNORDERED_SET stdext::hash_set
+using stdext::hash_set;
 #elif COMPILER == COMPILER_INTEL
-#define UNORDERED_MAP std::hash_map
-using std::hash_map;
+#define UNORDERED_SET std::hash_set
+using std::hash_set;
 #elif COMPILER == COMPILER_GNU && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
-#define UNORDERED_MAP std::tr1::unordered_map
+#define UNORDERED_SET std::tr1::unordered_set
 #elif COMPILER == COMPILER_GNU && __GNUC__ >= 3
-#define UNORDERED_MAP __gnu_cxx::hash_map
+#define UNORDERED_SET __gnu_cxx::hash_set
 
 #ifndef __HASH_STRUCTS__
 #define __HASH_STRUCTS__
@@ -77,7 +77,7 @@ namespace __gnu_cxx
 #endif // __HASH_STRUCTS__
 
 #else
-#define UNORDERED_MAP std::hash_map
-using std::hash_map;
+#define UNORDERED_SET std::hash_set
+using std::hash_set;
 #endif
 #endif
