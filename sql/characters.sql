ed5337ff5bebfe637b757e38eba1bedd2e668d5f
DasBlub <dasblub@gmail.com>
2010-09-12 22:06:58 +0200

    Drop unused column 'realmid' from the table 'character_tutorial'

--- sql/characters.sql
+++ sql/characters.sql
@@ -964,7 +964,6 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `character_tutorial`;
 CREATE TABLE `character_tutorial` (
   `account` bigint(20) unsigned NOT NULL auto_increment COMMENT 'Account Identifier',
-  `realmid` int(11) unsigned NOT NULL default '0' COMMENT 'Realm Identifier',
   `tut0` int(11) unsigned NOT NULL default '0',
   `tut1` int(11) unsigned NOT NULL default '0',
   `tut2` int(11) unsigned NOT NULL default '0',
---
+++ sql/updates/XXXXX_01_characters_character_tutorial.sql
@@ -0,0 +1,2 @@
+-- Drop no longer needed field
+ALTER TABLE character_tutorial DROP COLUMN realmid;
